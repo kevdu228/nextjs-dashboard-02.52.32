915da5e2874c11d8b3918f9df8a43872938bfc08
kevdu228 <bedinadekevin@hotmail.com>
2024-08-05 16:20:18 -0400

    font bug fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-import {lusitana} from '@/app/ui/font';
+import {lusitana} from '@/app/ui/fonts';
 import Image from 'next/image'
 
 export default function Page() {
